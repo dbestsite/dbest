35a545f4d4181193926cb799e05f1894f10083ef
dbestsite <paulhumilde06@gmail.com>
2025-05-14 20:00:14 +0800

    Update banner250-300.js

--- banner250-300.js
+++ banner250-300.js
@@ -1,13 +1,13 @@
-// ads.js
-var atOptions = {
-  'key': '5a944efcf8c1ae5899ea4d3ccf62e6a1',
-  'format': 'iframe',
-  'height': 250,
-  'width': 300,
-  'params': {}
+// banner250-300.js
+atOptions = {
+  'key' : '5a944efcf8c1ae5899ea4d3ccf62e6a1',
+  'format' : 'iframe',
+  'height' : 250,
+  'width' : 300,
+  'params' : {}
 };
 
-var s = document.createElement('script');
-s.type = 'text/javascript';
-s.src = '//swollenbasis.com/5a944efcf8c1ae5899ea4d3ccf62e6a1/invoke.js';
-document.body.appendChild(s);
+var adScript = document.createElement('script');
+adScript.type = 'text/javascript';
+adScript.src = '//swollenbasis.com/5a944efcf8c1ae5899ea4d3ccf62e6a1/invoke.js';
+document.body.appendChild(adScript);
