4d29a35174c192ff2d883b4671a6621f10ee2665
dbestsite <paulhumilde06@gmail.com>
2025-05-14 19:45:23 +0800

    Create banner250-300.js

---
+++ banner250-300.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '5a944efcf8c1ae5899ea4d3ccf62e6a1',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//swollenbasis.com/5a944efcf8c1ae5899ea4d3ccf62e6a1/invoke.js"></script>
